6c1a04ed7869098254550a6d6445e699f5b39bd8
Ashish <ashishdev@code.com>
2024-02-09 10:44:17 +0530

    adding mysql properties and removing h2 dependency

--- SpringBootDataJPA/src/main/java/com/datajpa/SpringBootDataJPA/SpringBootDataJpaApplication.java
+++ SpringBootDataJPA/src/main/java/com/datajpa/SpringBootDataJPA/SpringBootDataJpaApplication.java
@@ -3,8 +3,10 @@ package com.datajpa.SpringBootDataJPA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDataJpaApplication {
 
 	public static void main(String[] args) {
